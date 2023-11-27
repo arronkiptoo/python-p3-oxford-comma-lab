5efdd0aea97ce86be6fa63414f6324d12eda803e
arronkiptoo <kiptooaarron@gmail.com>
2023-11-27 22:16:00 +0300

    Completed Assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        # Join all items with commas, except the last one which gets "and" before it
+        return f"{', '.join(items[:-1])}, and {items[-1]}"
